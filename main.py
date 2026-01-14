5d39a496e5571659312232d28c4bd752dac8d951
smhamidi <1380hamidi@gmail.com>
2026-01-15 02:51:42 +0330

    Add: system tray icon and app icon.

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 import sys
 
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtGui
+
 
 from src.view import MainWindow
 from src.app_state import AppState
+from config.paths import ICON_PATH
 
 
 if __name__ == "__main__":
     app = QtWidgets.QApplication(sys.argv)
     app_state = AppState()
+
+    app.setWindowIcon(QtGui.QIcon(ICON_PATH))
 
     ui = MainWindow(app_state)
     ui.show()
