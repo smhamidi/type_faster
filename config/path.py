d74d7e19a8755c09319f06b0f3236ac72936369e
smhamidi <1380hamidi@gmail.com>
2026-02-25 19:13:50 +0330

    Ready for build using pyinstaller

--- config/path.py
+++ config/path.py
@@ -1,7 +1,11 @@
 import os
+import sys
 from pathlib import Path
 
-ROOT_DIR = str(Path(__file__).resolve().parent.parent)
+if getattr(sys, 'frozen', False) and hasattr(sys, '_MEIPASS'):
+    ROOT_DIR = sys._MEIPASS
+else:
+    ROOT_DIR = str(Path(__file__).resolve().parent.parent)
 
 _STATIC_DIR = os.path.join(ROOT_DIR, "static")
 ICON_PATH = os.path.join(_STATIC_DIR, "icon", "TypeFaster.png")
